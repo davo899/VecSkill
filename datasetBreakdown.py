cd8480e9f24fe7a6bedece89241dcc24399d5d46
djs21 <djs21@ic.ac.uk>
2024-02-04 16:41:20 +0000

    Added breakdown filters

--- datasetBreakdown.py
+++ datasetBreakdown.py
@@ -18,9 +18,10 @@ class Categoriser():
 categorisers = {
     "queue": Categoriser(lambda game: game["queueId"]),
     "gameMode": Categoriser(lambda game: game["gameMode"]),
-    "gameType": Categoriser(lambda game: game["gameType"])
+    "gameType": Categoriser(lambda game: game["gameType"]),
+    "gameVersion": Categoriser(lambda game: game["gameVersion"]),
+    "champion": Categoriser(lambda participant: participant["championName"])
 }
-
 
 with open("dbkey.txt", "r") as file:
     conn_string = file.readline()
@@ -32,14 +33,18 @@ with open("dbkey.txt", "r") as file:
     gameCount = 0
     while minId:
         cursor.execute(f"SELECT \"MatchJson\" FROM league.\"Match\" WHERE \"ID\" >= {minId} AND \"ID\" < {minId + 10000};")
-        gameInfo = [game[0]["info"] for game in cursor.fetchall()]
-        gameCount += len(gameInfo)
-        for game in gameInfo:
-            for categoriser in categorisers.values():
-                categoriser.add(game)
+        games = [game[0]["info"] for game in cursor.fetchall()]
+        games = [game for game in games if game["gameType"] == "MATCHED_GAME"] # Matched game type filter
+        games = [game for game in games if game["gameMode"] == "CLASSIC"] # Classic game mode filter
+        games = [game for game in games if game["queueId"] in (400, 420, 440)] # Draft Pick, Ranked Solo or Ranked Flex queue filter
+        
+        gameCount += len(games)
+        for game in games:
+            for participant in game["participants"]:
+                categorisers["champion"].add(participant)
 
         cursor.execute(f"SELECT MIN(\"ID\") FROM league.\"Match\" WHERE \"ID\" >= {minId + 10000};")
         minId = cursor.fetchall()[0][0]
         print(f"{gameCount} samples loaded")
 
-print(categorisers["gameType"].dict)
+print(categorisers["champion"].dict)
